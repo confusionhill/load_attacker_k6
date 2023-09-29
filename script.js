c718a0d44e5dcf3046b9af76899aebdd7d712f4f
Farhandika <18220015@std.stei.itb.ac.id>
2023-09-30 03:24:52 +0700

    update

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-function runLogin() {
-  const url = 'http://119.81.37.228:8080/api/user/login';
-  const payload = JSON.stringify({
-    name: 'ahmed rizandy',
-    password: 'zxcvbnm0'
-  });
-
-  const headers = {
-    'Accept': 'application/json, text/plain, */*',
-    'Content-Type': 'application/json;charset=UTF-8',
-    'Origin': 'http://119.81.37.228:8080',
-    'Referer': 'http://119.81.37.228:8080/login',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-  };
-
-  const response = http.post(url, payload, { headers });
-}
+const vmURL = 'http://119.81.37.228:8080'
+const kubeURL = 'http://45a15826-jp-tok.lb.appdomain.cloud:8080'
 
 function generateRandomString(length) {
   const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -30,8 +14,27 @@ function generateRandomString(length) {
   return randomString;
 }
 
-function runRegister() {
-  const url = 'http://119.81.37.228:8080/api/user/register';
+function runLogin(baseURL) {
+  const url = baseURL+'/api/user/login';
+  const payload = JSON.stringify({
+    name: 'ahmed rizandy',
+    password: 'zxcvbnm0'
+  });
+
+  const headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json;charset=UTF-8',
+    'Origin': baseURL,
+    'Referer': baseURL + '/login',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+  };
+
+  const response = http.post(url, payload, { headers });
+}
+
+function runRegister(baseURL) {
+  const url = baseURL + '/api/user/register';
   const payload = JSON.stringify({
     name: generateRandomString(10),
     email: generateRandomString(10)+"@email.com",
@@ -41,8 +44,8 @@ function runRegister() {
   const headers = {
     'Accept': 'application/json, text/plain, */*',
     'Content-Type': 'application/json;charset=UTF-8',
-    'Origin': 'http://119.81.37.228:8080',
-    'Referer': 'http://119.81.37.228:8080/login',
+    'Origin': baseURL,
+    'Referer': baseURL + '/login',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
   };
@@ -52,8 +55,10 @@ function runRegister() {
 
 export default function () {
   http.get("http://119.81.37.228:8080/api/catalogue/categories");
-  runLogin();
-  runRegister();
+  runLogin(vmURL);
+  runLogin(kubeURL)
+  runRegister(vmURL);
+  runRegister(kubeURL);
   sleep(1);
 }
 
